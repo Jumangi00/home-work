3439dadbb448353ebedb9519e951cbfe05704854
Jumangi00 <117804859+Jumangi00@users.noreply.github.com>
2024-10-15 20:11:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let Name = 'Maks'
+let age = '24'
+let Growth ='182'
+let Whoami='Man'
+let Hobby='Fishing'
+
+console.log(Name);
+console.log(age);
+console.log(Growth);
+console.log(Whoami);
+console.log(Hobby);
+
+
+
